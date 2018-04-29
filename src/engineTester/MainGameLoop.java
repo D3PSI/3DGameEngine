f14c4cff5411def2183ba13f158711a3d9348d58
Unknown <cedricschwyter@bluewin.ch>
2018-04-30 00:22:06 +0200

    First tryout model

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,43 @@
+package engineTester;
+
+import org.lwjgl.opengl.Display;
+
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
+
+public class MainGameLoop {
+
+	public static void main(String[] args) {
+		
+		DisplayManager.createDisplay();
+		
+		Loader loader = new Loader();
+		Renderer renderer = new Renderer();
+		
+		//OpenGL expect vertices to be defined counter clockwise by default
+		float[] vertices = {
+				//left bottom triangle
+				-0.5f, 0.5f, 0f,
+				-0.5f, -0.5f, 0f,
+				0.5f, -0,5f, 0f,
+				//right top triangle
+				0.5f, -0.5f, 0f,
+				0.5f, 0.5f, 0f,
+				-0.5f, 0.5f, 0f
+		};
+		
+		RawModel model = loader.loadToVAO(vertices);
+		
+		while(!Display.isCloseRequested()) {
+			renderer.prepare();
+			renderer.render(model);//render
+			DisplayManager.updateDisplay();
+		}
+		
+		loader.cleanUp();
+		DisplayManager.closeDisplay();
+	}
+
+}
