1f7eeccfb6e93097d563e19075703fbd1370c1dd
Unknown <cedricschwyter@bluewin.ch>
2018-04-30 20:09:05 +0200

    Transformation Matrix

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -2,12 +2,22 @@ package engineTester;
 
 import org.lwjgl.opengl.Display;
 
+import models.RawModel;
+import models.TexturedModel;
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
 import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
 
 public class MainGameLoop {
+	
+	/**
+	 * Loads up the position data for two triangles (which together make a quad)
+	 * into a VAO. This VAO is then rendered to the screen every frame.
+	 * 
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		
@@ -16,26 +26,41 @@ public class MainGameLoop {
 		Loader loader = new Loader();
 		Renderer renderer = new Renderer();
 		
+		StaticShader shader = new StaticShader();
+		
 		//OpenGL expect vertices to be defined counter clockwise by default
 		float[] vertices = {
-				//left bottom triangle
-				-0.5f, 0.5f, 0f,
-				-0.5f, -0.5f, 0f,
-				0.5f, -0,5f, 0f,
-				//right top triangle
-				0.5f, -0.5f, 0f,
-				0.5f, 0.5f, 0f,
-				-0.5f, 0.5f, 0f
+				-0.5f, 0.5f, 0,		//V0
+				-0.5f, -0.5f, 0,	//V1
+				0.5f, -0.5f, 0,		//V2
+				0.5f, 0.5f, 0		//V3
 		};
 		
-		RawModel model = loader.loadToVAO(vertices);
+		int[] indices = {
+				0, 1, 3,	//Top left triangle (V0, V1, V3)
+				3, 1, 2		//Bottom right triangle (V3, V1, V2)
+		};
+		
+		float[] textureCoords = {
+				0, 0,	//V0
+				0, 1,	//V1
+				1, 1,	//V2
+				1, 0	//V3
+		};
+		
+		RawModel model = loader.loadToVAO(vertices, textureCoords,indices);
+		ModelTexture texture = new ModelTexture(loader.loadTexture("image"));
+		TexturedModel texturedModel = new TexturedModel(model, texture);
 		
 		while(!Display.isCloseRequested()) {
 			renderer.prepare();
-			renderer.render(model);//render
+			shader.start();
+			renderer.render(texturedModel);//render
+			shader.stop();
 			DisplayManager.updateDisplay();
 		}
 		
+		shader.cleanUp();
 		loader.cleanUp();
 		DisplayManager.closeDisplay();
 	}
