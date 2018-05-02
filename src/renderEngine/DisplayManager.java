51e5df5c5bec458c81ca4c2788a41ec917ce3e47
Unknown <cedricschwyter@bluewin.ch>
2018-05-02 20:33:09 +0200

    Change in Display Dimensions

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -10,8 +10,8 @@ import org.lwjgl.opengl.PixelFormat;
 
 public class DisplayManager {
 	
-	private static final int WIDTH = 1720;
-	private static final int HEIGHT = 980;
+	private static final int WIDTH = 1900;
+	private static final int HEIGHT = 1060;
 	private static final int FPS_CAP = 120;
 	private static final String TITLE = "3D Game Engine" ;
 	
